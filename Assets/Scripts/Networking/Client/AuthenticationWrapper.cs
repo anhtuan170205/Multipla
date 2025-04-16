dd77d36ced79aeef6b3dee17c5378f5efddbb20e
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-04-16 17:00:15 +0700

    Authentication Refactor

--- Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
+++ Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
@@ -3,30 +3,72 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
 
 public static class AuthenticationWrapper
 {
     public static AuthState AuthState { get; private set; } = AuthState.NotAuthenticated;
-    public static async Task<AuthState> DoAuth(int tries = 5)
+    public static async Task<AuthState> DoAuth(int maxRetries = 5)
     {
         if (AuthState == AuthState.Authenticated)
         {
             return AuthState;
         }
+
+        if (AuthState == AuthState.Authenticating)
+        {
+            Debug.Log("Already authenticating...");
+            await Authenticating();
+            return AuthState;
+        }
+
+        await SignInAnonymouslyAsync(maxRetries);
+        return AuthState;
+    }
+
+    private static async Task<AuthState> Authenticating()
+    {
+        while (AuthState == AuthState.Authenticating || AuthState == AuthState.NotAuthenticated)
+        {
+            await Task.Delay(200);
+        }
+        return AuthState;
+    }
+
+    private static async Task SignInAnonymouslyAsync(int maxRetries = 5)
+    {
         AuthState = AuthState.Authenticating;
         int attempts = 0;
-        while (AuthState == AuthState.Authenticating && attempts < tries)
+        while (attempts < maxRetries)
         {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-            if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+            try
             {
-                AuthState = AuthState.Authenticated;
-                break;
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+                if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+                {
+                    AuthState = AuthState.Authenticated;
+                    break;
+                }
             }
+            catch (AuthenticationException ae)
+            {
+                AuthState = AuthState.Error;
+                Debug.LogError(ae);
+            }
+            catch (RequestFailedException rfe)
+            {
+                AuthState = AuthState.Error;
+                Debug.LogError(rfe);
+            }
+            
             attempts++;
             await Task.Delay(1000);
         }
-        return AuthState;
+        if (AuthState != AuthState.Authenticated)
+        {
+            AuthState = AuthState.TimeOut;
+            Debug.LogWarning("Authentication timed out.");
+        }
     }
 }
 
