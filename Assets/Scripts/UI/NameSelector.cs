26830df49ad83460dd186df66a91e0a2e894fd25
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-04-20 15:37:08 +0700

    Select Name

---
+++ Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class NameSelector : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameInputField;
+    [SerializeField] private Button connectButton;
+    [SerializeField] private int minNameLength = 1;
+    [SerializeField] private int maxNameLength = 12;
+    private const string PlayerNameKey = "PlayerName";
+    private void Start()
+    {
+        if (SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+        nameInputField.text = PlayerPrefs.GetString(PlayerNameKey, string.Empty);
+        HandleNameChanged();
+    }
+    
+    public void HandleNameChanged()
+    {
+        connectButton.interactable = nameInputField.text.Length >= minNameLength && nameInputField.text.Length <= maxNameLength;
+    }
+
+    public void Connect()
+    {
+        PlayerPrefs.SetString(PlayerNameKey, nameInputField.text);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
