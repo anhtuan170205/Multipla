98d8f5f1aed81d9923f920722db87fd08ee48961
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-05-07 12:57:56 +0700

    Random Spawn Points

---
+++ Assets/Scripts/Core/SpawnPoint.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    private static List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
+
+    private void OnEnable()
+    {
+        spawnPoints.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        spawnPoints.Remove(this);
+    }
+
+    public static Vector3 GetRandomSpawnPos()
+    {
+        if (spawnPoints.Count == 0)
+        {
+            return Vector3.zero;
+        }
+        int randomIndex = Random.Range(0, spawnPoints.Count);
+        return spawnPoints[randomIndex].transform.position;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 1f);
+    }
+}
--- Assets/Scripts/Networking/Server/NetworkServer.cs
+++ Assets/Scripts/Networking/Server/NetworkServer.cs
@@ -27,6 +27,8 @@ public class NetworkServer : IDisposable
         authIdToUserData[userData.userAuthId] = userData;
 
         response.Approved = true;
+        response.Position = SpawnPoint.GetRandomSpawnPos();
+        response.Rotation = Quaternion.identity;
         response.CreatePlayerObject = true;
     }
     private void OnNetworkReady()
