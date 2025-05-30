95ebe6ddc41f1e5b7c41f9e96f714aef6a4798da
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-05-30 22:34:34 +0700

    Bounty Coins (Adjusted)

---
+++ Assets/Scripts/Core/Coin/BountyCoin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BountyCoin : Coin
+{
+    public override int Collect()
+    {
+        if (!IsServer)
+        {
+            Show(false);
+            return 0;
+        }
+        if (isCollected) { return 0; }
+        isCollected = true;
+        Destroy(gameObject);
+        return coinValue;
+    }
+}
