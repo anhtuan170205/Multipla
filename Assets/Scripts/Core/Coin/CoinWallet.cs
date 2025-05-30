f67c881ed434384fa7c3e8b3b9794f3c2b36ca46
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-05-30 17:51:46 +0700

    Revert "Bounty Coins"
    This reverts commit 3ea184b34af240e5c5da7472ac7fb41ca3e580ad.

--- Assets/Scripts/Core/Coin/CoinWallet.cs
+++ Assets/Scripts/Core/Coin/CoinWallet.cs
@@ -5,55 +5,7 @@ using Unity.Netcode;
 
 public class CoinWallet : NetworkBehaviour
 {
-    [Header("References")]
-    [SerializeField] private Health health;
-    [SerializeField] private BountyCoin bountyCoinPrefab;
-
-    [Header("Settings")]
-    [SerializeField] private float bountyCoinSpread = 3f;
-    [SerializeField] private float bountyPercentageOnDeath = 50f;
-    [SerializeField] private int bountyCoinCount = 10;
-    [SerializeField] private int minBountyCoinValue = 5;
-    [SerializeField] private LayerMask layerMask;
-    private float coinRadius;
-    private Collider2D[] coinBuffer = new Collider2D[1];
-
     public NetworkVariable<int> TotalCoins = new NetworkVariable<int>();
-
-    public override void OnNetworkSpawn()
-    {
-        if (!IsServer)
-        {
-            return;
-        }
-        coinRadius = bountyCoinPrefab.GetComponent<CircleCollider2D>().radius;
-        health.OnDied += HandleDie;
-
-    }
-    public override void OnNetworkDespawn()
-    {
-        if (!IsServer)
-        {
-            return;
-        }
-        health.OnDied -= HandleDie;
-    }
-
-    public void HandleDie(Health health)
-    {
-        int bountyValue = TotalCoins.Value * Mathf.RoundToInt(bountyPercentageOnDeath / 100f);
-        int bountyCoinValue = bountyValue / bountyCoinCount;
-        if (bountyCoinValue < minBountyCoinValue)
-        {
-            return;
-        }
-        for (int i = 0; i < bountyCoinCount; i++)
-        {
-            BountyCoin bountyCoinInstance = Instantiate(bountyCoinPrefab, GetSpawnPoint(), Quaternion.identity);
-            bountyCoinInstance.SetValue(bountyCoinValue);
-            bountyCoinInstance.GetComponent<NetworkObject>().Spawn();
-        }
-    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -72,18 +24,5 @@ public class CoinWallet : NetworkBehaviour
     public void SpendCoins(int amount)
     {
         TotalCoins.Value -= amount;
-    }
-
-    private Vector2 GetSpawnPoint()
-    {
-        while (true)
-        {
-            Vector2 spawnPoint = (Vector2)transform.position + Random.insideUnitCircle * bountyCoinSpread;
-            int numColliders = Physics2D.OverlapCircleNonAlloc(spawnPoint, coinRadius, coinBuffer, layerMask);
-            if (numColliders == 0)
-            {
-                return spawnPoint;
-            }
-        }
     }
 }
